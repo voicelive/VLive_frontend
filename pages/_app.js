c1bc904546bc6d2e3e708a0f2e46fd0053e7ae06
eunbin <eunbines2@gmail.com>
2021-09-05 16:30:32 +0900

    inital setting

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import PropTypes from "prop-types";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object,
+};
