2d49c2efe17abdff87a0742aaa82cfc5ed3043f4
soldonii <hyunsol.do@tosspayments.com>
2021-09-06 22:07:37 +0900

    eslint 에러코드 제거

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,8 +1,6 @@
 import UserProfile from '../../src/components/UserProfile';
 
 export default function Main() {
-  const test = '123';
-
   return (
     <div>
       <div>Main Page(채널 리스트 페이지)</div>
