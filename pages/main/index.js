8f14ba2ea24855f287f6ddbe42dd15982ef96f55
soldonii <hyunsol.do@tosspayments.com>
2021-09-06 22:33:23 +0900

    useSWR 사용법1

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,6 +1,15 @@
+import useSWR from 'swr';
 import UserProfile from '../../src/components/UserProfile';
 
 export default function Main() {
+  const { data } = useSWR('/todos/1', () => {
+    return fetch('https://jsonplaceholder.typicode.com/todos/1').then(
+      (response) => response.json(),
+    );
+  });
+
+  console.log('data', data);
+
   return (
     <div>
       <div>Main Page(채널 리스트 페이지)</div>
