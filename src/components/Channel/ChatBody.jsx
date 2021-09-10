606e0cc65a687de6c3ead5f793e303d69cad32b9
Hayun <mycolki@gmail.com>
2021-09-11 00:53:40 +0900

    feat: set api url to env

--- src/components/Channel/ChatBody.jsx
+++ src/components/Channel/ChatBody.jsx
@@ -34,7 +34,7 @@ export default function ChatBody({ channelId }) {
 }
 
 ChatBody.propTypes = {
-  channelId: PropTypes.string.isRequired,
+  channelId: PropTypes.string.,
 };
 
 const Contents = styled.div`
