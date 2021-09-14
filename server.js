ce3cbbcbbcb5fabaa4e3edf91d71cbf8908ccc0d
eunbin <eunbines2@gmail.com>
2021-09-14 09:48:34 +0900

    feat: socket events of server for webRTC connection

--- server.js
+++ server.js
@@ -10,67 +10,49 @@ const nextHandler = nextApp.getRequestHandler();
 
 const { EVENTS } = require('./src/constants/socketEvent');
 
-let users = {};
+const users = {};
+
+const socketToChannel = {};
 
 io.on('connection', (socket) => {
   console.log('socket connected...');
 
   socket.on(EVENTS.CREATE_CHANNEL, (channel) => {
-    users[channel._id] = [];
     socket.broadcast.emit(EVENTS.LISTEN_CREATE_CHANNEL, channel);
   });
 
   socket.on(EVENTS.ENTER_CHANNEL, (userData) => {
-    socket.join(userData.channelId);
+    const { channelId } = userData;
 
-    if (!users[userData.channelId]) {
-      users[userData.channelId] = [];
+    if (users[channelId]) {
+      users[channelId].push(socket.id);
+    } else {
+      users[channelId] = [socket.id];
     }
+    socketToChannel[socket.id] = channelId;
 
-    users[userData.channelId].push({
-      [socket.id]: userData,
-    });
+    const usersInThisChannel = users[channelId].filter(
+      (id) => id !== socket.id,
+    );
 
-    const newUser = {
-      ...userData,
-      socketId: socket.id,
-    };
+    socket.emit('all users', usersInThisChannel);
 
-    socket.broadcast.emit(EVENTS.LISTEN_ENTER_CHANNEL, newUser);
+    socket.join(channelId);
+
+    socket.broadcast.emit(EVENTS.LISTEN_ENTER_CHANNEL, userData);
   });
 
-  socket.on(EVENTS.SEND_SIGNAL, ({ signal, receiver }) => {
-    let initiator;
-    for (const channel in users) {
-      [initiator] = users[channel].filter(
-        (user) => Object.keys(user)[0] === socket.id,
-      );
-    }
-
-    const [initiatorId, initiatorInfo] = Object.entries(initiator)[0];
-
-    const { socketId } = receiver;
-    io.to(socketId).emit(EVENTS.LISTEN_SEND_SIGNAL, {
-      initiatorId,
-      initiatorInfo,
-      signal,
+  socket.on(EVENTS.SENDING_SIGNAL, ({ userToSignal, callerId, signal }) => {
+    io.to(userToSignal).emit(EVENTS.USER_JOINED, {
+      signal: signal,
+      callerId: callerId,
     });
   });
 
-  socket.on(EVENTS.RETURN_SIGNAL, ({ signal, receiverId }) => {
-    let returner;
-
-    for (const channel in users) {
-      [returner] = users[channel].filter(
-        (user) => Object.keys(user)[0] === socket.id,
-      );
-    }
-    const [returnerId, returnerInfo] = Object.entries(returner)[0];
-
-    io.to(receiverId).emit(EVENTS.LISTEN_RETURN_SIGNAL, {
-      returnerId,
-      returnerInfo,
-      signal,
+  socket.on(EVENTS.RETURNING_SIGNAL, ({ callerId, signal }) => {
+    io.to(callerId).emit(EVENTS.RECEIVING_RETURNED_SIGNAL, {
+      signal: signal,
+      id: socket.id,
     });
   });
 
@@ -90,6 +72,16 @@ io.on('connection', (socket) => {
 
   socket.on(EVENTS.PLAYER_READY, ({ channelId, _id, userRole }) => {
     io.to(channelId).emit(EVENTS.LISTEN_PLAYER_READY, { _id, userRole });
+  });
+
+  socket.on('disconnect', () => {
+    const channelId = socketToChannel[socket.id];
+    let channel = users[channelId];
+
+    if (channel) {
+      channel = channel.filter((id) => id !== socket.id);
+      users[channelId] = channel;
+    }
   });
 });
 
