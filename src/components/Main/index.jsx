708043d2aa393dea5c38a01c18f063961eaf6063
Hayun <mycolki@gmail.com>
2021-09-15 21:42:04 +0900

    remove unnecessarty import

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import theme from '../../styles/theme';
 
 import Header from '../Header';
 import UserProfile from './UserProfile';
