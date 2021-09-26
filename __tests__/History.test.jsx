a9af778a79d7c7fe263d1d4a4503d0e567f1c20e
julian-jeong <julian-jeong@github.com>
2021-09-27 02:03:48 +0900

    refactor: history component test

--- __tests__/History.test.jsx
+++ __tests__/History.test.jsx
@@ -4,67 +4,16 @@ import { render } from '@testing-library/react';
 
 import History from '../src/components/History';
 
-jest.mock('../src/hooks/channel/useChannel', () => ({
+jest.mock('../src/components/History/RecentHistory', () => ({
   __esModule: true,
   default: function () {
-    return {
-      channel,
-      error: null,
-    };
+    return <h2>RecentHistory</h2>;
   },
-}));
-
-const channel = {
-  name: '아무나 들어오세요',
-  episode: { title: '타짜' },
-  players: [
-    {
-      user: {
-        email: 'abc@gmail.com',
-        name: 'jon doe',
-        photoUrl: 'https://picsum.photos/200/300',
-        _id: '1234qwe',
-      },
-      voteCount: 1,
-      character: {
-        imgUrl: 'https://picsum.photos/70/70',
-        name: 'a',
-        _id: '1234qwe0',
-      },
-      _id: '1234qwe1',
-    },
-    {
-      user: {
-        email: '1234@gmail.com',
-        name: 'jon',
-        photoUrl: 'https://picsum.photos/200/300',
-        _id: '1234qwe2',
-      },
-      voteCount: 0,
-      character: {
-        imgUrl: 'https://picsum.photos/70/70',
-        name: 'b',
-        _id: '1234qwe3',
-      },
-      _id: '1234qwe4',
-    },
-  ],
-};
-
-nextRouter.useRouter = jest.fn();
-nextRouter.useRouter.mockImplementation(() => ({
-  query: { historyId: '012345' },
 }));
 
 describe('History', () => {
   it('renders History component', () => {
     const { getByText } = render(<History />);
-    expect(getByText(channel.name)).toBeInTheDocument();
-
-    channel.players.map((player) => {
-      expect(getByText(player.user.name)).toBeVisible();
-      expect(getByText(player.character.name)).toBeVisible();
-      expect(getByText(player.voteCount)).toBeVisible();
-    });
+    expect(getByText('RecentHistory')).toBeInTheDocument();
   });
 });
